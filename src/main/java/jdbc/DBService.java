caec4f604ae25ace0478af9546d38bee493967cf
MrBlackBear <proboostbiznes@gmail.com>
2019-01-19 00:28:13 +0300

    First commit

---
+++ src/main/java/jdbc/DBService.java
@@ -0,0 +1,76 @@
+package jdbc;
+
+import jdbc.dao.UsersDAO;
+import model.User;
+import org.h2.jdbcx.JdbcDataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBService {
+    private final Connection connection;
+
+    public DBService() {
+        this.connection = getH2Connection();
+    }
+
+    public User getUser(long id) throws DBException {
+        try {
+            return (new UsersDAO(connection).get(id));
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+
+    public long addUser(String name) throws DBException {
+        try {
+            connection.setAutoCommit(false);
+            UsersDAO dao = new UsersDAO(connection);
+            dao.createTable();
+            dao.insertUser(name);
+            connection.commit();
+            return dao.getUserId(name);
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignore) {
+            }
+            throw new DBException(e);
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ignore) {
+            }
+        }
+    }
+
+    public void cleanUp() throws DBException {
+        UsersDAO dao = new UsersDAO(connection);
+        try {
+            dao.dropTable();
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+
+
+    public static Connection getH2Connection() {
+        try {
+            String url = "jdbc:h2:./h2db";
+            String name = "users";
+            String pass = "12345";
+
+            JdbcDataSource ds = new JdbcDataSource();
+            ds.setURL(url);
+            ds.setUser(name);
+            ds.setPassword(pass);
+
+            Connection connection = DriverManager.getConnection(url, name, pass);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
